3b10651605caa058021a51176fdcab567cda9743
sandi <sandimac@users.noreply.github.com>
2015-11-18 20:43:55 -0800

    completed question 3

--- 02-range-and-loops/for_fun_loops.py
+++ 02-range-and-loops/for_fun_loops.py
@@ -1,8 +1,11 @@
 # for i in range(1,21):
 #     print i,
 
-for i in range(1,21):
-    if i==13:
-        print "Hello",
-    else:
-        print i,
+# for i in range(1,21):
+#     if i==13:
+#         print "Hello",
+#     else:
+#         print i,
+
+for i in range(0,101,10):
+    print i,
